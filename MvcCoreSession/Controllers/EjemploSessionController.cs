e7700d67d22bdc96db9e6cdb839824585aa3c2bb
serraguti <pacoserranox@gmail.com>
2023-03-01 11:12:04 +0100

    commit

--- MvcCoreSession/Controllers/EjemploSessionController.cs
+++ MvcCoreSession/Controllers/EjemploSessionController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreSession.Helpers;
+using MvcCoreSession.Models;
 
 namespace MvcCoreSession.Controllers
 {
@@ -14,15 +16,55 @@ namespace MvcCoreSession.Controllers
 
         public IActionResult SessionSimple(string accion)
         {
-            this.numero += 1;
-            ViewData["NUMERO"] = "Valor del número: " + this.numero;
-            //if (accion.ToLower() == "almacenar")
-            //{
+            if (accion != null)
+            {
+                if (accion.ToLower() == "almacenar")
+                {
+                    HttpContext.Session.SetString("nombre", "Programeitor");
+                    HttpContext.Session.SetString("hora",
+                        DateTime.Now.ToLongTimeString());
+                    ViewData["MENSAJE"] = "Datos almacenados en Session";
+                }
+                else if (accion.ToLower() == "mostrar")
+                {
+                    ViewData["USUARIO"] =
+                        HttpContext.Session.GetString("nombre");
+                    ViewData["HORA"] =
+                        HttpContext.Session.GetString("hora");
+                }
+            }
+            return View();
+        }
 
-            //}else if (accion.ToLower() == "mostrar")
-            //{
-
-            //}
+        public IActionResult SessionPersona(string accion)
+        {
+            if (accion != null)
+            {
+                if (accion.ToLower() == "almacenar")
+                {
+                    Persona persona = new Persona();
+                    persona.Nombre = "Alumno";
+                    persona.Email = "alumno@gmail.com";
+                    persona.Edad = 25;
+                    //DEBEMOS CONVERTIR EL OBJETO PERSONA
+                    //A BYTE[] PARA ALMACENARLO EN SESSION
+                    byte[] data = 
+                        HelperBinarySession.ObjectToByte(persona);
+                    //ALMACENAMOS EL OBJETO BINARIO EN SESSION
+                    HttpContext.Session.Set("PERSONA", data);
+                    ViewData["MENSAJE"] = "Datos almacenados";
+                }
+                else if (accion.ToLower() == "mostrar")
+                {
+                    //EXTRAEMOS EL OBJETO PERSONA DESDE LOS BYTES[]
+                    //DE SESSION
+                    byte[] data = HttpContext.Session.Get("PERSONA");
+                    //CONVERTIMOS EL BINARIO A OBJETO
+                    Persona persona = (Persona)
+                        HelperBinarySession.ByteToObject(data);
+                    ViewData["PERSONA"] = persona;
+                }
+            }
             return View();
         }
     }
