950ba5c2565e3f5461532a8f5c90bd3fd86e258a
serraguti <pacoserranox@gmail.com>
2023-03-01 10:03:43 +0100

    Add project files.

---
+++ MvcCoreSession/Controllers/EjemploSessionController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcCoreSession.Controllers
+{
+    public class EjemploSessionController : Controller
+    {
+        int numero = 1;
+
+        public IActionResult Index()
+        {
+            ViewData["NUMERO"] = "Valor del número: " + this.numero;
+            return View();
+        }
+
+        public IActionResult SessionSimple(string accion)
+        {
+            this.numero += 1;
+            ViewData["NUMERO"] = "Valor del número: " + this.numero;
+            //if (accion.ToLower() == "almacenar")
+            //{
+
+            //}else if (accion.ToLower() == "mostrar")
+            //{
+
+            //}
+            return View();
+        }
+    }
+}
